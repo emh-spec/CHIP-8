f8f0c09eb49cf1f29ff7c7bd181cf4085b869459
M. Mainul <mainul.m@outlook.com>
2026-04-22 12:23:38 +0600

    Added display width and height constant

--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,7 @@
 pub const Chip8 = @import("chip8.zig").Chip8;
+
+/// Display width value (`64`px).
+pub const DISPLAY_WIDTH: usize = 64;
+
+/// Display height value (`32`px).
+pub const DISPLAY_HEIGHT: usize = 32;
