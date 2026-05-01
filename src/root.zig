54d1a3fab42947c4e8bcb9caccde2adda732a7d8
M. Mainul <mainul.m@outlook.com>
2026-05-02 00:57:46 +0600

    included the `display` and `input`

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,9 @@
 pub const Chip8 = @import("chip8.zig").Chip8;
 
+pub const Display = @import("display.zig").Display;
+
+pub const Input = @import("input.zig").Input;
+
 /// Display width value (`64`px).
 pub const DISPLAY_WIDTH: usize = 64;
 
